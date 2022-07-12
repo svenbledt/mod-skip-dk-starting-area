d1129554b891fa099f007f4ba24bb3f23b15b5b7
acidmanifesto <joshua.lee.betts@gmail.com>
2022-07-12 08:37:47 -0400

    Update conf.sh

--- conf/conf.sh
+++ conf/conf.sh
@@ -3,9 +3,7 @@
 ## CUSTOM SQL - Important file used by the db_assembler.sh
 ## Keep only the required variables (base sql files or updates, depending on the DB)
 
-
 ## UPDATE SQL
-
 DB_WORLD_CUSTOM_PATHS+=(
-	"$MOD_SKIP_DK_STARTING_AREA_ROOT"/sql/db-world"
+	$MOD_SKIP_DK_STARTING_AREA_ROOT"/sql/db-world/"
 )
